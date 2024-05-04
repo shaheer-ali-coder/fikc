ee0ff31cde1fc10398b808c699365e365acba0ec
shaheer-ali-coder <130545001+shaheer-ali-coder@users.noreply.github.com>
2024-05-04 07:48:44 -0700

    f
    f

--- index.js
+++ index.js
@@ -50,58 +50,56 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, 
   `7 Green Flags: ✅✅✅✅✅✅✅ (look for all 7)
 
-📛 Name: CatCoin | Ticker $CatCoin
-📋 CA: b6euDF3ihA3Beh32e3duedye7edgbZzeyw (Sol Chain)
-
-💲 Price: $0.00281023
-💸 MC: $274k | Liq: $33.4k (12%)
-📉 Taxes: B: 0% | S: 0% | T: 0% 
- 👜 MaxTx: None | MaxW: N/A
-📊Transactions: Buys: 123 Sells: 22
-🪂 Airdrops: 6 for a total of 10%
-✖️ Mintable Yes or No (✅👎 or ❌👍)
-🔥 LP Burned = 🔥 or ❌
-🔒LP Locked = 🔒 or ❌ (111 days)
-
-✅ Contract Verified 
-🔎 Etherscan - Yes or No (+ link)
-🔎 Solscan - 👍 or 👎 (+ link) - both will have emojis like this one)
-
-✅ Renounced Ownership
-Owner (1. Wallet address + 2. Quantity of Tokens + 3. % of supply)
-for example...
-🧑‍💻Deployer --eJUm | Q: 144.5m | P: 14.45%
-👤 Owner: RENOUCED
-⚙️ Chain: SOL | ⏳Age: 22m (minutes)
-
-✅ Website + Socials
-🌐 Website: (+ hyperlink)
-📥 Telegram (+ hyperlink)
-🐦 Twitter (+ hyperlink)
-
-✅ Top 10 Holders
-H1: 📦(1. Wallet address with 4 letter hyperlink + 2. Quantity of Tokens + 3. % of supply)
-H2: 📦 - for example...
-H3: 📦 --eJUm + hyperlink | Q: 144.5m | P: 14.45%
-H4: 📦 --Ng5U + hyperlink | Q: 11.21m | P: 1.12%
-H5: 📦 --vxCv + hyperlink | Q: 6.2m | P: 0.62%
-H6: 📦(+ link) -- etc, etc 
-H7: 📦(+ link) + (all wallet addresses need to have clickable links)
-H8: 📦(+ link)
-H9: 📦(+ link)
-H10📦: (+ link)
-
-✅ First 10 Transactions:
-T1: 📈 (1. wallet address with 4 letter hyperlink + 2. Buy amount of Token + 3. Sell amount of Token)
-T2: 📈 for example...
-T3: 📈 --eJUm + hyperlink | B:50 sol | S: 3 sol
-T4: 📈 --Ng5U + hyperlink B:0 sol | S:4.7 sol
-T5: 📈 --vxCv + hyperlink | B:4.5 sol | S:17.7 sol
-T6: 📈(+ link) -- etc, etc 
-T7: 📈(+ link) + (all wallet addresses need to have clickable links)
-T8: 📈(+ link)
-T9: 📈(+ link)
-T10: 📈(+ link)`
+  📛 Name: CatCoin | Ticker $CatCoin
+  📋 CA: b6euDF3ihA3Beh32e3duedye7edgbZzeyw (Sol Chain)
+  
+  💲 Price: $0.00281023
+  💸 MC: $274k | Liq: $33.4k (12%)
+  📉 Taxes: B: 0% | S: 0% | T: 0% 
+   👜 MaxTx: None | MaxW: N/A
+  📊Transactions: Buys: 123 Sells: 22
+  🪂 Airdrops: 6 for a total of 10%
+  ✖️ Mintable ✅👎 or ❌👍
+  🔥 LP Burned = 🔥 or ❌
+  🔒LP Locked = 🔒 or ❌ 
+  
+  ✅ Contract Verified 
+  🔎 Etherscan - 👍 or 👎 
+  🔎 Solscan - 👍 or 👎 
+  
+  ✅ Renounced Ownership
+  🧑‍💻Deployer --eJUm | Q: 144.5m | P: 14.45%
+  👤 Owner: RENOUCED
+  ⚙️ Chain: SOL | ⏳Age: 22m (minutes)
+  
+  ✅ Website + Socials
+  🌐 Website: (+ hyperlink)
+  📥 Telegram (+ hyperlink)
+  🐦 Twitter (+ hyperlink)
+  
+  ✅ Top 10 Holders
+  H1: 📦 --eJUm | Q: 144.5m | P: 14.45%
+  H2: 📦 --eJUm | Q: 144.5m | P: 14.45%
+  H3: 📦 --eJUm | Q: 144.5m | P: 14.45%
+  H4: 📦 --Ng5U | Q: 11.21m | P: 1.12%
+  H5: 📦 --vxCv  | Q: 6.2m | P: 0.62%
+  H6: 📦 --eJUm | Q: 144.5m | P: 14.45%
+  H7: 📦 --eJUm | Q: 144.5m | P: 14.45%
+  H8: 📦  --eJUm | Q: 144.5m | P: 14.45%
+  H9: 📦  --eJUm | Q: 144.5m | P: 14.45%
+  H10📦  --eJUm | Q: 144.5m | P: 14.45%
+  
+  ✅ First 10 Transactions:
+  T1: 📈 --eJUm | B:50 sol | S: 3 sol
+  T2: 📈 --eJUm | B:50 sol | S: 3 sol
+  T3: 📈 --eJUm | B:50 sol | S: 3 sol
+  T4: 📈 --Ng5U | B:0 sol | S:4.7 sol
+  T5: 📈 --vxCv | B:4.5 sol | S:17.7 sol
+  T6: 📈 --eJUm | B:50 sol | S: 3 sol
+  T7: 📈 --eJUm | B:50 sol | S: 3 sol
+  T8: 📈 --eJUm | B:50 sol | S: 3 sol
+  T9: 📈 --eJUm | B:50 sol | S: 3 sol
+  T10: 📈 --eJUm | B:50 sol | S: 3 sol`
     );
 });
 
